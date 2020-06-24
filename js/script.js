21677c3aa6bc85cb99806445fb6d44ca77201112
endaytrer <endaytrer@gmail.com>
2020-06-25 00:25:43 +0800

    add cache saving & custom/random code content

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $("document").ready(function () {
     var qrCode = new QRCode("qrCode", {
-        text: "               " + "AC5BFE246FA1451C1BD85C6B2AEDCFAC5BFE246FA1451C1BD85C6B2AEDC" + "               ",
+        text: "               " + app.qrCode + "               ",
         width: 150,
         height: 150,
         colorDark: "#f97afb",
