8558f7ef37eac28ccb23dc2cfc009806898f16bf
endaytrer <endaytrer@gmail.com>
2020-07-04 00:03:32 +0800

    update 7.3

--- js/script.js
+++ js/script.js
@@ -3,10 +3,37 @@ $("document").ready(function () {
         text: "               " + app.qrCode + "               ",
         width: 150,
         height: 150,
-        colorDark: "#f97afb",
+        colorDark: app.colorOfCode,
         correctLevel: QRCode.CorrectLevel.H
     });
     $("#menuToggler").click(function () {
         $(".floatMenu").toggle("fast");
+        qrCode.clear();
+        document.getElementById("qrCode").innerHTML = "";
+        if (app.qrCode === "") {
+            app.qrCode = app.generateCode();
+        }
+        qrCode = new QRCode("qrCode", {
+            text: "               " + app.qrCode + "               ",
+            width: 150,
+            height: 150,
+            colorDark: app.colorOfCode,
+            correctLevel: QRCode.CorrectLevel.H
+        });
+    });
+    $(".submitButton").click(function () {
+        $(".floatMenu").toggle("fast");
+        qrCode.clear();
+        document.getElementById("qrCode").innerHTML = "";
+        if (app.qrCode === "") {
+            app.qrCode = app.generateCode();
+        }
+        qrCode = new QRCode("qrCode", {
+            text: "               " + app.qrCode + "               ",
+            width: 150,
+            height: 150,
+            colorDark: app.colorOfCode,
+            correctLevel: QRCode.CorrectLevel.H
+        });
     });
 });
