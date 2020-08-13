f7b6def4ea767b945d48e6af6c1e088e70d3ed27
endaytrer <endaytrer@gmail.com>
2020-08-14 02:09:58 +0800

    remove useless code fragment

--- js/script.js
+++ js/script.js
@@ -36,19 +36,4 @@ $("document").ready(function () {
             correctLevel: QRCode.CorrectLevel.H
         });
     });
-    $("#saveUrlButton").click(function () {
-        $(".floatMenu").toggle("fast");
-        qrCode.clear();
-        document.getElementById("qrCode").innerHTML = "";
-        if (app.qrCode === "") {
-            app.qrCode = app.generateCode();
-        }
-        qrCode = new QRCode("qrCode", {
-            text: "               " + app.qrCode + "               ",
-            width: 150,
-            height: 150,
-            colorDark: app.colorOfCode,
-            correctLevel: QRCode.CorrectLevel.H
-        });
-    });
 });
