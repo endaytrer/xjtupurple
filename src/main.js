96acb6db2390f763ed83051d53aeb957d0f25b4c
endaytrer <endaytrer@gmail.com>
2021-01-14 18:34:21 +0800

    remove due to regulations

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from './router'
-
-const app = createApp(App).use(router);
-app.mount("#app");
--- vue.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  devServer: {
-    proxy: "http://hello.xjtu.edu.cn",
-  },
-};
